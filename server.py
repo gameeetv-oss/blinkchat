572566bee57f6edb5cf821891f18765f4714419c
gameeetv <gameeetv@gmail.com>
2026-03-10 23:01:34 +0300

    Google Search Console dogrulama dosyasi eklendi

--- server.py
+++ server.py
@@ -169,6 +169,7 @@ async def main():
     app.router.add_get("/", index)
     app.router.add_get("/ping", ping)
     app.router.add_get("/ws", ws_handler)
+    app.router.add_get("/googlee2b500dcde5fee75.html", lambda r: web.FileResponse(BASE / "static" / "googlee2b500dcde5fee75.html"))
     app.router.add_static("/icons", BASE / "static" / "icons")
     app.router.add_static("/static", BASE / "static")
     runner = web.AppRunner(app)
